02e9dd535ab7f7eec4c32a2805a48fc9a6e6565d
Calum Grant <calumgrant@github.com>
2024-12-26 10:53:43 +0000

    WIP

--- Fractals.cpp
+++ Fractals.cpp
@@ -4,7 +4,11 @@
 
 void register_fractals(fractals::Registry &r) {
   r.add(mandelbrot_fractal);
-  r.add(cubic_mandelbrot_fractal);
+  r.add(mandelbrot3_fractal);
+  r.add(mandelbrot4_fractal);
+  r.add(mandelbrot5_fractal);
+  r.add(mandelbrot6_fractal);
+  r.add(mandelbrot7_fractal);
   r.add(mandeldrop_fractal);
 
   // To add custom fractals, insert them into this list, for example:
