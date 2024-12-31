c4633feeeaae6c6174c12e7253da7dbb5a271b55
Calum Grant <calumgrant@github.com>
2024-12-31 08:27:50 +0000

    Rewrite RenderingSequence for threading

--- RenderingSequence.hpp
+++ RenderingSequence.hpp
@@ -4,17 +4,38 @@ namespace fractals {
 // Visits all points (x,y) in the region of size w*h
 class RenderingSequence {
 public:
-  RenderingSequence(int w, int h, int stride = 128)
-      : width(w), height(h), initial_stride(stride), stride(stride), x(0),
-        y(0) {}
+  RenderingSequence() = default; // Invalid state do not use
+  RenderingSequence(int w, int h, int stride)
+      : width(w), height(h), initial_stride(stride), stride(0), x(0), y(0) {}
 
-  const int width, height;
-  int x, y; // Current
-  const int initial_stride;
-  int stride;
+  void reset() { stride = 0; }
 
+  // Gets the next pixel in the sequence
+  // Returns true if succeeded, or false if the sequence is done
+  // We can call next() multiple times at the end
+  bool next(int &out_x, int &out_y, int &out_stride, bool &out_stride_changed) {
+    out_stride_changed = false;
+    if (stride == 0) {
+      stride = initial_stride;
+      out_x = x = 0;
+      out_y = y = 0;
+      out_stride = stride;
+      return true;
+    }
+    while (next0(out_stride_changed)) {
+      if (!already_done()) {
+        out_x = x;
+        out_y = y;
+        out_stride = stride;
+        return true;
+      }
+    }
+    return false;
+  }
+
+private:
   // Visit all points using the stride
-  bool next0() {
+  bool next0(bool &out_stride_changed) {
     x += stride;
     if (x >= width) {
       x = 0;
@@ -22,6 +43,7 @@ public:
       if (y >= height) {
         if (stride == 1)
           return false;
+        out_stride_changed = true;
         stride = stride / 2;
         x = 0;
         y = 0;
@@ -41,13 +63,10 @@ public:
     return !(x & mask) && !(y & mask);
   }
 
-  bool next() {
-    while (next0()) {
-      if (!already_done())
-        return true;
-    }
-    return false;
-  }
+  int width, height;
+  int x, y; // Current
+  int initial_stride;
+  int stride;
 };
 
 } // namespace fractals
