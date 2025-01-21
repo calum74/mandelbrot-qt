0bbf3ae519f75b819d2ae8c1bdd6bbb05392e358
Calum Grant <calumgrant@github.com>
2025-01-21 09:18:26 +0000

    Tidying

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -4,14 +4,15 @@
 #include "Viewport.hpp"
 #include "mandelbrot.hpp"
 
-using namespace fractals;
-
 // !! Not used
+
+namespace fractals {
 RGB blend(RGB c1, RGB c2, int w1, int w2) {
   return make_rgb((red(c1) * w1 + red(c2) * w2) / (w1 + w2),
                   (green(c1) * w1 + green(c2) * w2) / (w1 + w2),
                   (blue(c1) * w1 + blue(c2) * w2) / (w1 + w2));
 }
+} // namespace fractals
 
 void fractals::interpolate_region(Viewport &vp, int x0, int y0, int h) {
   auto c00 = vp(x0, y0);
@@ -42,39 +43,40 @@ void fractals::interpolate_region(Viewport &vp, int x0, int y0, int h) {
     }
 }
 
-void Renderer::increase_iterations(Viewport &) {}
+void fractals::Renderer::increase_iterations(Viewport &) {}
 
-void Renderer::decrease_iterations(Viewport &) {}
+void fractals::Renderer::decrease_iterations(Viewport &) {}
 
-void Renderer::center(Viewport &vp) {}
+void fractals::Renderer::center(Viewport &vp) {}
 
-void Renderer::zoom_in(Viewport &vp) {}
+void fractals::Renderer::zoom_in(Viewport &vp) {}
 
-void Renderer::redraw(Viewport &vp) {
+void fractals::Renderer::redraw(Viewport &vp) {
   for (int j = 0; j < vp.height; ++j)
     for (int i = 0; i < vp.width; ++i)
       vp(i, j) = with_extra(vp(i, j), 127);
 }
 
-void Renderer::enable_auto_depth(bool value) {}
+void fractals::Renderer::enable_auto_depth(bool value) {}
 
-void Renderer::set_threading(int threads) {}
+void fractals::Renderer::set_threading(int threads) {}
 
-void Renderer::get_depth_range(double &min, double &p, double &max) {}
+void fractals::Renderer::get_depth_range(double &min, double &p, double &max) {}
 
-void Renderer::calculate_async(fractals::Viewport &view, const ColourMap &cm) {}
+void fractals::Renderer::calculate_async(fractals::Viewport &view,
+                                         const ColourMap &cm) {}
 
-double Renderer::get_average_iterations() const { return 0; }
+double fractals::Renderer::get_average_iterations() const { return 0; }
 
-double Renderer::get_average_skipped_iterations() const { return 0; }
+double fractals::Renderer::get_average_skipped_iterations() const { return 0; }
 
-void Viewport::region_updated(int x, int y, int w, int h) {}
+void fractals::Viewport::region_updated(int x, int y, int w, int h) {}
 
-void Viewport::discovered_depth(int, double) {}
+void fractals::Viewport::discovered_depth(int, double) {}
 
 void fractals::Viewport::finished(double, int, int, double, double, double) {}
 
-void Renderer::discovered_depth(int, double) {}
+void fractals::Renderer::discovered_depth(int, double) {}
 
 void fractals::Renderer::set_fractal(const fractals::PointwiseFractal &) {}
 
@@ -85,9 +87,9 @@ void fractals::Viewport::invalidateAllPixels() {
     }
 }
 
-void Renderer::load(const view_parameters &params, Viewport &vp) {}
+void fractals::Renderer::load(const view_parameters &params, Viewport &vp) {}
 
-void Renderer::save(view_parameters &params) const {}
+void fractals::Renderer::save(view_parameters &params) const {}
 
 std::unique_ptr<fractals::Renderer> fractals::make_renderer(Registry &reg) {
   return std::make_unique<AsyncRenderer>(mandelbrot_fractal, reg);
